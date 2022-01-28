68af7853ec735a386b5e98307f0fd786f2839554
valera <mikolyuk28@gmail.com>
2022-01-28 23:03:28 +0100

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -7,27 +7,88 @@ window.addEventListener('DOMContentLoaded', () => {
     // burger(); triggerButton, menuClass, activeClass
     Sliders()
 
-    // cookies
-
-    // const modal = document.querySelector('.cookies-block'),
-    //     modalBottom = document.querySelector('.cookies-bottom'),
-    //     modalCloseButton = document.querySelector('.cookies-block__icon'),
-    //     modalBottomCloseButton = document.querySelector('.cookies-bottom__button');
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows());
+        }
+    };
+    
+    if (isMobile.any()) {
+        document.body.classList.add('_touch');
+    
+        let menuArrows = document.querySelectorAll('.menu__arrow');
+        if (menuArrows.length > 0) {
+            for (let index = 0; index < menuArrows.length; index++) {
+                const menuArrow = menuArrows[index];
+                menuArrow.addEventListener("click", function (e) {
+                    menuArrow.parentElement.classList.toggle('_active');
+                });
+            }
+        }
+    
+    } else {
+        document.body.classList.add('_pc');
+    }
+    
+    // Меню бургер
+    const iconMenu = document.querySelector('.menu__icon');
+    const menuBody = document.querySelector('.menu__body');
+    if (iconMenu) {
+        iconMenu.addEventListener("click", function (e) {
+            document.body.classList.toggle('_lock');
+            iconMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        });
+    }
     
     
-    // function openModal() {
-    //     modal.classList.toggle('_active');
-    //     modalBottom.classList.toggle('_active');
-    //     clearInterval(openModal);
-    // }
-
-    // function closeModal() {
-    //     modal.classList.toggle('_active');
-    //     modalBottom.classList.toggle('_active');
-    // }
-    // modalCloseButton.addEventListener('click', closeModal);
-    // modalBottomCloseButton.addEventListener('click', closeModal);
-    // const openTimeSet = setTimeout(openModal, 2000);
+    // Прокрутка при клике
+    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
+    
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+    
+                if (iconMenu.classList.contains('_active')) {
+                    document.body.classList.remove('_lock');
+                    iconMenu.classList.remove('_active');
+                    menuBody.classList.remove('_active');
+                }
+    
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+    }
 
     
     
--- app/js/shop.js
+++ app/js/shop.js
@@ -2,29 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     "use strict";
 
 
-//hearts
-// const iconHeart = document.querySelectorAll('.item-column__heart');
-    
-// iconHeart.forEach(heart => {
-//     heart.addEventListener('click', () => {
-//         changeHeart(heart);  
-//     });
-// });
-// function changeHeart(element) {
-//     element.classList.toggle('fas');   
-// }
 
-//filters
-   
-// const filtersButton = document.querySelector('#filters-button'),
-//       filtersBody = document.querySelector('.filters-menu');
-
-// filtersButton.addEventListener('click', () => {
-//     openFilters();
-// });
-// function openFilters() {
-//     filtersBody.classList.toggle('active');
-// }
     
 })
 
